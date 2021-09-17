16c73112c633b39cb8f78e0aa5d5221e7d01850c
JP STUNTS <contact@cybersamurai.dk>
2021-09-17 13:42:52 +0200

    First push

---
+++ Controllers/Page1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDesignDEMOsite.Controllers
+{
+    public class Page1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Page1/index.cshtml");
+        }
+    }
+}
--- wwwroot/js/gamefile.js
+++ wwwroot/js/gamefile.js
@@ -8,37 +8,37 @@ function dicegame() {
 
     function dice1(){ 
         if (randomNumber1 == 1) {  
-            $(".img1").attr("src", "~/img/dice1.png");
+            $(".img1").attr("src", "../../img/dice1.png");
             value1 = 1;
             return value1;
         }
 
         else if (randomNumber1 == 2) {  
-            document.querySelector(".img1").setAttribute("src", "~/img/dice2.png");
+            document.querySelector(".img1").setAttribute("src", "../../img/dice2.png");
             value1 = 2;
             return value1;
         }
 
         else if (randomNumber1 == 3) {  
-            document.querySelector(".img1").setAttribute("src", "~/img/dice3.png");
+            document.querySelector(".img1").setAttribute("src", "../../img/dice3.png");
             value1 = 3;
             return value1;
         }
 
         else if (randomNumber1 == 4) {  
-            document.querySelector(".img1").setAttribute("src", "~/img/dice4.png")
+            document.querySelector(".img1").setAttribute("src", "../../img/dice4.png")
             value1 = 4;
             return value1;
         }
 
         else if (randomNumber1 == 5) {  
-            document.querySelector(".img1").setAttribute("src", "~/img/dice5.png")
+            document.querySelector(".img1").setAttribute("src", "../../img/dice5.png")
             value1 = 5;
             return value1;
         }
 
         else if (randomNumber1 == 6) {  
-            document.querySelector(".img1").setAttribute("src", "~/img/dice6.png")
+            document.querySelector(".img1").setAttribute("src", "../../img/dice6.png")
             value1 = 6;
             return value1;
         }
@@ -48,37 +48,37 @@ function dicegame() {
 
     function dice2(){
         if (randomNumber2 == 1) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice1.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice1.png")
             value2 = 1;
             return value2;
         }
 
         else if (randomNumber2 == 2) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice2.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice2.png")
             value2 = 2;
             return value2;
         }
 
         else if (randomNumber2 == 3) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice3.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice3.png")
             value2 = 3;
             return value2;
         }
 
         else if (randomNumber2 == 4) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice4.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice4.png")
             value2 = 4;
             return value2;
         }
 
         else if (randomNumber2 == 5) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice5.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice5.png")
             value2 = 5;
             return value2;
         }
         
         else if (randomNumber2 == 6) {  
-            document.querySelector(".img2").setAttribute("src", "~/img/dice6.png")
+            document.querySelector(".img2").setAttribute("src", "../../img/dice6.png")
             value2 = 6;
             return value2;
         }
